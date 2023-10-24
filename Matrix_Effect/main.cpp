fd17b8b625bc327c3bbd457aa6f61688747be439
JasonDouma <109489317+JasonDouma@users.noreply.github.com>
2023-10-24 13:00:10 -0200

    Add files via upload

---
+++ Matrix_Effect/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome To The Matrix" << std::endl;
+    std::cout << "\n";
+    std::cout << "Matrix Will Now Begin" << std::endl;
+    std::cout << "\n";
+    while (true)
+    {
+      int one = 1;
+      int two = 2;
+      std::cout << one << std::endl;
+      std::cout << "                     " << two << std::endl;
+      std::cout << "                                             " << one << std::endl;
+      std::cout << "                                                                                   " << two << std::endl;
+      std::cout << "                                                                                                           " << one << std::endl;
+      std::cout << "                                                                                                                                                                                                                      " << two << std::endl;
+      std::cout << "                                                                                                                                                                                                                                                                                                                                                                                                                                            " << one << std::endl;
+      std::cout << "                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        " << two << std::endl;
+
+    }
+    return 0;
+}
